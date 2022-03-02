0a78ee5ac15324d9f9eacd67c47e8736898939f2
Ryan Vickramasinghe <rvickram@uwo.ca>
2022-03-01 22:47:31 -0500

    Parse command line args and connect to a server.

--- Client/GetImage.js
+++ Client/GetImage.js
@@ -2,11 +2,35 @@ let net = require("net");
 let fs = require("fs");
 let open = require("open");
 
-// let ITPpacket = require("./ITPRequest"); // uncomment this line after you run npm install command
+let ITPpacket = require("./ITPRequest"); // uncomment this line after you run npm install command
 
 // Enter your code for the client functionality here
 
+const args = parseArgs(process.argv);
 
+// Attempt to connect
+const client = new net.Socket();
+client.connect({port : args.port, host: args.host});
+console.log()
+
+
+// Parse the command line args
+function parseArgs(args) {
+    if (args.length != 8) {
+        console.error("Invalid arguments specified!");
+    } else {
+        const serverInfo = args[3].split(":");
+        const imageName = args[5];
+        const version = parseInt(args[7]);
+
+        return {
+            host: serverInfo[0],
+            port: parseInt(serverInfo[1]),
+            imageName,
+            version
+        }
+    }
+}
 
 
 
@@ -17,28 +41,28 @@ let open = require("open");
 function parseBitPacket(packet, offset, length) {
     let number = "";
     for (var i = 0; i < length; i++) {
-      // let us get the actual byte position of the offset
-      let bytePosition = Math.floor((offset + i) / 8);
-      let bitPosition = 7 - ((offset + i) % 8);
-      let bit = (packet[bytePosition] >> bitPosition) % 2;
-      number = (number << 1) | bit;
+        // let us get the actual byte position of the offset
+        let bytePosition = Math.floor((offset + i) / 8);
+        let bitPosition = 7 - ((offset + i) % 8);
+        let bit = (packet[bytePosition] >> bitPosition) % 2;
+        number = (number << 1) | bit;
     }
     return number;
-  }
-  
-  // Prints the entire packet in bits format
-  function printPacketBit(packet) {
+}
+
+// Prints the entire packet in bits format
+function printPacketBit(packet) {
     var bitString = "";
-  
+
     for (var i = 0; i < packet.length; i++) {
-      // To add leading zeros
-      var b = "00000000" + packet[i].toString(2);
-      // To print 4 bytes per line
-      if (i > 0 && i % 4 == 0) bitString += "\n";
-      bitString += " " + b.substr(b.length - 8);
+        // To add leading zeros
+        var b = "00000000" + packet[i].toString(2);
+        // To print 4 bytes per line
+        if (i > 0 && i % 4 == 0) bitString += "\n";
+        bitString += " " + b.substr(b.length - 8);
     }
     console.log(bitString);
-  }
+}
 
 
-  
+
